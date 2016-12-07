6f1bfc03fd5ce3d97c8f7481002539c84c61bc93
Nattanon-Bunyatipanon <nattanon33@yahoo.com>
2016-12-07 02:40:14 -0800

    Quiz Bug

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
 import Qcontroller from './QuizController';
-// import $ from 'jquery';
+import $ from 'jquery';
+import Question from './question';
 
 // The page that will actually be rendered on the site
 class QuizPage extends React.Component {
@@ -26,6 +27,9 @@ class QuizPage extends React.Component {
                     qObj['stateRank'] = data.state_rank;
                     qObj['phoneNum'] = data.phone;
                     qObj['birthday'] = data.birthday;
+                    qObj['questions'] = ["What is this official's phone number?",
+                        "When is this official's birthday?",
+                        "What is this official's State Rank?"]
                     return qObj;
                 })
                 thisComponent.setState({ legislatorInfo: formatQ })
@@ -44,26 +48,28 @@ class QuizPage extends React.Component {
                     <h2>Test How Well You Know Your Legislators!</h2>
                     <p> To get started, please enter your zipcode </p>
                     <SearchForm fetchData={this.fetchData} />
-                    <Quiz legislatorInfo={this.state.legislatorInfo} />
+                    <QuizCont legislatorInfo={this.state.legislatorInfo} fetchData={this.fetchData} />
                 </main>
             </div>
         );
     }
 }
 
-class Quiz extends React.Component {
+class QuizOff extends React.Component {
     render() {
         return (
-                <div id='container'>
-                    <br />
-                    <div id='quiz'></div>
-                    <div class='button' id='next'><a href='#'>Next</a></div>
-                    <div class='button' id='prev'><a href='#'>Prev</a></div>
-                    <div class='button' id='start'> <a href='#'>Start Over</a></div>
-                </div >
+            <div id='container'>
+                <br />
+                <div id='quiz'></div>
+                <h2> Click search to start! </h2>
+                <div class='button' id='next'><a href='#'>Next</a></div>
+                <div class='button' id='prev'><a href='#'>Prev</a></div>
+                <div class='button' id='start'> <a href='#'>Start Over</a></div>
+            </div >
         );
     }
 }
+
 
 // Search bar that passes query to app
 class SearchForm extends React.Component {
@@ -95,69 +101,125 @@ class SearchForm extends React.Component {
     }
 }
 
-class QuizControl extends React.Component {
+class Quiz extends React.Component {
+  constructor(props) {
+      super(props)
+      this.state = {
+        quiz: {},
+        index: 0,
+        answers: []
+      }
+  }
+
+  componentDidMount() {
+    $.getJSON('./Quiz.json', function(result) {
+      this.setState({quiz: result})
+    }.bind(this))
+  }
+
+  handleSubmit() {
+    if (this.state.index < this.state.quiz.questions.length) {
+      this.setState({'index': this.state.index + 1})
+    } else {
+      let score = this.state.score || 0
+      this.state.answers.map((answer, i) => (
+        score = score + this.state.quiz.questions[i].answers[answer].point
+      ))
+      this.setState({'score': score})
+    }
+  }
+
+  handleAnswerSelected(event) {
+    let list = [...this.state.answers.slice(0, this.state.index),
+                parseInt(event.target.value),
+                ...this.state.answers.slice(this.state.index + 1)]
+    this.setState({'answers': list})
+  }
+
+  render() {
+    const {
+      quiz, index, answers
+    } = this.state
+
+    let completed = (quiz.questions && (index === quiz.questions.length)) ? true : false
+    let numberOfQuestions = quiz.questions ? quiz.questions.length : 0
+    let score = 0
+    if (completed) {
+      this.state.answers.map((answer, i) => (
+        score = score + this.state.quiz.questions[i].answers[answer].point
+      ))
+    }
+
+    return (
+      <div>
+        <h1>{quiz.title}</h1>
+        {completed ?
+          <div>
+            <p>Congratulation, you finished the quiz</p>
+            Your score is {score}
+          </div>
+        :
+          <div>
+          <h2>Question {index + 1} of {numberOfQuestions}</h2>
+          {quiz.questions && index < numberOfQuestions ?
+            <Question
+              question={quiz.questions[index]}
+              index={index}
+              onAnswerSelected={(event) => this.handleAnswerSelected(event)}
+              onSubmit={() => this.handleSubmit()}
+            />
+          : ''}
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+class QuizCont extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            quiz: {},
+            quiz: [{ firstName: 'Maria', lastName: 'Cantwell' }, { second: '2' }],
+            firstName: 'Maria',
+            lastName: 'Cantwell',
+            phoneNum: '202-224-3441',
+            birthday: '1997-06-06',
+            stateRank: 'senior',
             index: 0,
             answers: []
         }
     }
 
-    handleSubmit() {
-        if (this.state.index < this.state.quiz.questions.length) {
-            this.setState({ 'index': this.state.index + 1 })
-        } else {
-            let score = this.state.score || 0
-            this.state.answers.map((answer, i) => (
-                score = score + this.state.quiz.questions[i].answers[answer].point
-            ))
-            this.setState({ 'score': score })
-        }
+    fetchData(searchTerm) {
+        var thisComponent = this;
+        Qcontroller.searchLegislators(searchTerm)
+            .then(function (data) {
+                thisComponent.setState({
+                    index: 0,
+                    quiz: this.props.legislatorInfo,
+                    answer: []
+                })
+            })
+        console.log(this.state.quiz);
     }
 
-    handleAnswerSelected(event) {
-        let list = [...this.state.answers.slice(0, this.state.index),
-        parseInt(event.target.value),
-        ...this.state.answers.slice(this.state.index + 1)]
-        this.setState({ 'answers': list })
+    handleNext() {
+
+    }
+
+    handleSubmit() {
+
     }
 
     render() {
-        const {
-            quiz, index, answers
-        } = this.state
-
-        let completed = (quiz.questions && (index === quiz.questions.length)) ? true : false
-        let numberOfQuestions = quiz.questions ? quiz.questions.length : 0
-        let score = 0
-        if (completed) {
-            this.state.answers.map((answer, i) => (
-                score = score + this.state.quiz.questions[i].answers[answer].point
-            ))
-        }
         return (
             <div>
-                <h2>{quiz.title}</h2>
-                {completed ?
-                    <div>
-                        <p>Congratulation, you finished the quiz</p>
-                        Your score is {score}
-                    </div>
-                    :
-                    <div>
-                        <h2>Question {index + 1} of {numberOfQuestions}</h2>
-                        {quiz.questions && index < numberOfQuestions ?
-                            <Question
-                                question={quiz.questions[index]}
-                                index={index}
-                                onAnswerSelected={(event) => this.handleAnswerSelected(event)}
-                                onSubmit={() => this.handleSubmit()}
-                                />
-                            : ''}
-                    </div>
-                }
+                <h3 class="kn"> Current Official: {this.state.quiz[this.state.index].firstName} {this.state.quiz[this.state.index].lastName}</h3>
+                <h4 class='knn'> Q1: What is this official's state rank? </h4>
+                <input onChange={this.handleChange} type='text' className='form-control' placeholder='Type in your answer here'></input>
+                <button className="subbuttons" onClick={this.handleNext}>Next</button>
+                <button className="subbuttons" onClick={this.handleSubmit}>Submit</button>
             </div>
         )
     }
