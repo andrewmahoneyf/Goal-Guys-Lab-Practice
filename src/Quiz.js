8c3db0a0c91506154bb0652de227627b82ffc6a4
Nattanon-Bunyatipanon <nattanon33@yahoo.com>
2016-12-07 15:07:47 -0800

    Tried fixing quiz code

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
 import Qcontroller from './QuizController';
-import $ from 'jquery';
-import Question from './question';
+
+// This page was meant for voters to test their knowledge on their elected officials to make sure
+// their values align, and not voting simply because the official's voice is the loudest amongst their peers
+// There is a bug in my code that I can't seem to figure out which is not passing my prop (legislatorInfo) down to
+// the child component properly. I have tried to tackle this problem for quite a while but it has been of no use.
+// I'm sorry I could not finish this page by the deadline - Nattanon Bunyatipanon
+
 
 // The page that will actually be rendered on the site
 class QuizPage extends React.Component {
@@ -18,9 +23,9 @@ class QuizPage extends React.Component {
     fetchData(searchTerm) {
         var thisComponent = this;
         Qcontroller.searchLegislators(searchTerm)
-            .then(function (data) {
+            .then(function(data) {
                 console.log(data);
-                var formatQ = data.results.map(function (data) {
+                var formatQ = data.results.map(function(data) {
                     var qObj = {};
                     qObj['firstName'] = data.first_name;
                     qObj['lastName'] = data.last_name;
@@ -32,7 +37,7 @@ class QuizPage extends React.Component {
                         "What is this official's State Rank?"]
                     return qObj;
                 })
-                thisComponent.setState({ legislatorInfo: formatQ })
+                thisComponent.setState({ legislatorInfo: formatQ }, () => { console.log('state set'); console.log(this.state) })
                 console.log("this");
             })
             .catch((err) => this.setState({ legislatorInfo: [], searchValue: '' }));
@@ -41,6 +46,7 @@ class QuizPage extends React.Component {
     // Renders the html elements in the webapp
     render() {
         // making sure they are mapping
+        console.log('in render')
         console.log(this.state.legislatorInfo);
         return (
             <div>
@@ -48,24 +54,9 @@ class QuizPage extends React.Component {
                     <h2>Test How Well You Know Your Legislators!</h2>
                     <p> To get started, please enter your zipcode </p>
                     <SearchForm fetchData={this.fetchData} />
-                    <QuizCont legislatorInfo={this.state.legislatorInfo} fetchData={this.fetchData} />
+                    <QuizCont legislatorInformation={this.state.legislatorInfo} />
                 </main>
             </div>
-        );
-    }
-}
-
-class QuizOff extends React.Component {
-    render() {
-        return (
-            <div id='container'>
-                <br />
-                <div id='quiz'></div>
-                <h2> Click search to start! </h2>
-                <div class='button' id='next'><a href='#'>Next</a></div>
-                <div class='button' id='prev'><a href='#'>Prev</a></div>
-                <div class='button' id='start'> <a href='#'>Start Over</a></div>
-            </div >
         );
     }
 }
@@ -101,111 +92,57 @@ class SearchForm extends React.Component {
     }
 }
 
-class Quiz extends React.Component {
-  constructor(props) {
-      super(props)
-      this.state = {
-        quiz: {},
-        index: 0,
-        answers: []
-      }
-  }
-
-  componentDidMount() {
-    $.getJSON('./Quiz.json', function(result) {
-      this.setState({quiz: result})
-    }.bind(this))
-  }
-
-  handleSubmit() {
-    if (this.state.index < this.state.quiz.questions.length) {
-      this.setState({'index': this.state.index + 1})
-    } else {
-      let score = this.state.score || 0
-      this.state.answers.map((answer, i) => (
-        score = score + this.state.quiz.questions[i].answers[answer].point
-      ))
-      this.setState({'score': score})
-    }
-  }
-
-  handleAnswerSelected(event) {
-    let list = [...this.state.answers.slice(0, this.state.index),
-                parseInt(event.target.value),
-                ...this.state.answers.slice(this.state.index + 1)]
-    this.setState({'answers': list})
-  }
-
-  render() {
-    const {
-      quiz, index, answers
-    } = this.state
-
-    let completed = (quiz.questions && (index === quiz.questions.length)) ? true : false
-    let numberOfQuestions = quiz.questions ? quiz.questions.length : 0
-    let score = 0
-    if (completed) {
-      this.state.answers.map((answer, i) => (
-        score = score + this.state.quiz.questions[i].answers[answer].point
-      ))
-    }
-
-    return (
-      <div>
-        <h1>{quiz.title}</h1>
-        {completed ?
-          <div>
-            <p>Congratulation, you finished the quiz</p>
-            Your score is {score}
-          </div>
-        :
-          <div>
-          <h2>Question {index + 1} of {numberOfQuestions}</h2>
-          {quiz.questions && index < numberOfQuestions ?
-            <Question
-              question={quiz.questions[index]}
-              index={index}
-              onAnswerSelected={(event) => this.handleAnswerSelected(event)}
-              onSubmit={() => this.handleSubmit()}
-            />
-          : ''}
-          </div>
-        }
-      </div>
-    )
-  }
-}
-
 class QuizCont extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            quiz: [{ firstName: 'Maria', lastName: 'Cantwell' }, { second: '2' }],
+            quiz: [{ firstName: 'Maria', 
+                     lastName: 'Cantwell', 
+                     questions: ['What is this official\'s State Rank?', 'When is this official\'s birthday?'] }, 
+                     { firstName: 'Patty', 
+                       lastName: 'Murray',
+                       questions: ['What is this official\'s State Rank?', 'When is this official\'s birthday?']
+                    }],
             firstName: 'Maria',
             lastName: 'Cantwell',
             phoneNum: '202-224-3441',
             birthday: '1997-06-06',
             stateRank: 'senior',
-            index: 0,
+            indexQ: 0,
+            indexP: 0,
             answers: []
         }
+        this.handleStart = this.handleStart.bind(this);
+        this.handleNext = this.handleNext.bind(this);
     }
 
-    fetchData(searchTerm) {
+    handleStart() {
+        console.log('in handle start')
+        console.log(this.props)
         var thisComponent = this;
-        Qcontroller.searchLegislators(searchTerm)
-            .then(function (data) {
-                thisComponent.setState({
-                    index: 0,
-                    quiz: this.props.legislatorInfo,
-                    answer: []
-                })
-            })
-        console.log(this.state.quiz);
+        thisComponent.setState({
+            indexQ: 0,
+            indexP: 0,
+            quiz: this.props.legislatorInformation,
+            firstName: 'Maria',
+            lastName: 'Cantwell',
+            phoneNum: '202-224-3441',
+            birthday: '1997-06-06',
+            stateRank: 'senior',
+            answer: []
+        })
+        console.log(this.state.quiz, "quiz info");
     }
 
-    handleNext() {
-
+    // When clicking next, the question is updated. Once Questions are through, 
+    handleNext(text) {
+        this.state.answers.push(text);
+        if (this.state.indexQ < this.state.quiz[0].questions.length) {
+            this.setState( {indexQ: this.state.indexQ++} );
+        }
+        else {
+            this.setState( {indexP: this.state.indexP++, indexQ: 0} );
+        }
     }
 
     handleSubmit() {
@@ -213,13 +150,21 @@ class QuizCont extends React.Component {
     }
 
     render() {
+        // To check why the props are not being passed down properly
+        console.log(this.props.legislatorInformation, "checking prop");
+        var quizz = this.props.legislatorInformation;
+        console.log(this.quizz, "this quizz")
+        console.log(this.state.quiz, "state of quiz");
+
         return (
             <div>
-                <h3 class="kn"> Current Official: {this.state.quiz[this.state.index].firstName} {this.state.quiz[this.state.index].lastName}</h3>
-                <h4 class='knn'> Q1: What is this official's state rank? </h4>
-                <input onChange={this.handleChange} type='text' className='form-control' placeholder='Type in your answer here'></input>
+                <h3 class="kn"> Current Official: {this.state.quiz[0].firstName} {this.state.quiz[0].lastName}</h3>
+                <h4 class='knn'> {this.state.quiz[this.state.indexQ].questions[this.state.indexQ]} </h4>
+                <input onChange={this.handleSubmit} type='text' className='form-control' placeholder='Type in your answer here'></input>
+                <button className="subbuttons" onClick={this.handleStart}>Start</button>
                 <button className="subbuttons" onClick={this.handleNext}>Next</button>
                 <button className="subbuttons" onClick={this.handleSubmit}>Submit</button>
+
             </div>
         )
     }
